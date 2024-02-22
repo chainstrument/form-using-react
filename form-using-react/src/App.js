eeaf3fc29d12535eac0f1b657f278be1a333b451
Chahir <minitutoriel@gmail.com>
2024-02-22 22:39:49 +0100

    form

--- form-using-react/src/App.js
+++ form-using-react/src/App.js
@@ -2,9 +2,7 @@
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/passoword.png'
+
 
 function App() {
   return (
