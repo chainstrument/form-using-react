b6b19df56c0a619963cce1bc4da8f237aa2cf63b
Chahir <minitutoriel@gmail.com>
2024-02-22 22:25:33 +0100

    init

---
+++ form-using-react/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/passoword.png'
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
---
+++ form-using-react/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/passoword.png'
+
+const LoginSignup = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text"></div>
+        </div>
+    </div>
+  )
+}
+
+
+export default LoginSignup
